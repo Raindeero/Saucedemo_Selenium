ca41ca8e51452dca4dce894b25cc6f56bf18de4f
Raindeero <raindeero@mail.ru>
2024-07-05 16:16:20 +0500

    auth

--- main.py
+++ main.py
@@ -22,7 +22,25 @@ wait = WebDriverWait(driver, 15, poll_frequency=1)
 
 driver.get(URL)
 
-wait.until(EC.element_to_be_clickable(INPUT_USERNAME)).send_keys(LOGIN)
-wait.until(EC.element_to_be_clickable(INPUT_PASSWORD)).send_keys(PASSWORD)
-wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
+
+def wwait(*args):
+    '''
+    Просто хотел сократить количество кода, вот и сделал функцию под самые частые строчки
+    '''
+    if len(args) == 1:
+        web_element = args[0]
+        wait.until(EC.element_to_be_clickable(web_element)).click()
+        time.sleep(3)
+    elif len(args) == 2:
+        web_element, keys = args
+        wait.until(EC.element_to_be_clickable(web_element)).send_keys(keys)
+        time.sleep(3)
+    else:
+        raise ValueError("Too many arguments, expect one or two.")
+
+
+# Authorization as standard user
+wwait(INPUT_USERNAME, LOGIN)
+wwait(INPUT_PASSWORD, PASSWORD)
+wwait(LOGIN_BUTTON)
 time.sleep(3)
--- variables.py
+++ variables.py
@@ -4,4 +4,16 @@ PASSWORD = "secret_sauce"
 
 INPUT_USERNAME = ('xpath', '//input[@id="user-name"]')
 INPUT_PASSWORD = ('xpath', '//input[@id="password"]')
-LOGIN_BUTTON = ('xpath', '//button[@id="login-button"]')
+LOGIN_BUTTON = ('xpath', '//input[@id="login-button"]')
+
+# Burger menu section
+BURGER_BUTTON = ('xpath', '//button[@id="react-burger-menu-btn"]')
+CLOSE_BURGER_BUTTON = ('xpath', '//button[@id="react-burger-cross-btn"]')
+ALL_ITEMS_BUTTON = ('xpath', '//button[@id="inventory_sidebar_link"]')
+ABOUT_BUTTON = ('xpath', '//button[@id="about_sidebar_link"]')
+LOGOUT_BUTTON = ('xpath', '//button[@id="logout_sidebar_link"]')
+RESET_APP_STATE_BUTTON = ('xpath', '//button[@id="reset_sidebar_link"]')
+
+KART_LINK = ('xpath', '//a[@class="shopping_cart_link"]')
+
+SELECT_FILTER = ('xpath', '//select[@class="product_sort_container"]')
